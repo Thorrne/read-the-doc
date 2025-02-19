a5da0155333b716504b88c5664dfe17a021dc4f8
eyuuab <eyobedabreham9@gmail.com>
2025-02-19 23:51:49 +0300

    fix: structure

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-extensions = [
-    "myst_parser",  # Enables Markdown support
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode"
-]
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# Recognize Markdown files
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
+project = 'MeTTa Standard Library'
+copyright = '2025, Eyobed A.'
+author = 'Eyobed A.'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,12 +1,17 @@
-.. MeTTa Standard Library documentation master file
+.. MeTTa Standard Library documentation master file, created by
+   sphinx-quickstart on Wed Feb 19 23:51:16 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-Welcome to MeTTa Standard Library's documentation!
-===================================================
+MeTTa Standard Library documentation
+====================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   equality_and_reduction.md
-   error_handling.md
-   evaluation_control.md
